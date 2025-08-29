65731d01618d31167db051194b5455042f291676
geo61 <geo61rus@yandex.ru>
2025-08-29 14:38:06 +0300

    Фаза 5: Добавлен кастомный Toolbar на экран чата

--- app/src/main/java/com/andrey/hybridchat/ChatActivity.kt
+++ app/src/main/java/com/andrey/hybridchat/ChatActivity.kt
@@ -2,10 +2,23 @@ package com.andrey.hybridchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        // Находим нашу новую панель в макете
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        // Назначаем ее главной панелью для этого экрана
+        setSupportActionBar(toolbar)
+
+        val intent = intent
+        val receiverUid = intent.getStringExtra("USER_UID")
+        val receiverEmail = intent.getStringExtra("USER_EMAIL")
+
+        // Теперь эта команда будет работать с НАШЕЙ панелью и гарантированно установит заголовок
+        supportActionBar?.title = receiverEmail
     }
 }
